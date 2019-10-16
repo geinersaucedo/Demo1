e5c2f630f45aefb31d48acb20f667376afe01765
geinersaucedo <gsaucedo92@gmail.com>
2019-10-16 17:28:32 -0500

    Test1

---
+++ WebApiNetCoreEnterpriseLibrary/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace WebApiNetCoreEnterpriseLibrary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        Database _localDbDefaul;
+
+        public WeatherForecastController()
+        {
+  
+            DatabaseProviderFactory providerFactoryDefault = new DatabaseProviderFactory();
+            _localDbDefaul = providerFactoryDefault.Create("DefaultConnection");
+        }
+        public object getUsuarioSistema()
+        {
+
+            object[] Parameters = new object[] { };
+            IRowMapper<Usuario> EntidadFileMapper = MapBuilder<Usuario>
+                .MapNoProperties()
+                .MapByName(prop => prop.CodRol)
+                .MapByName(prop => prop.CodFuncion)
+                .MapByName(prop => prop.DescripRol)
+                 .Build();
+
+            var listEntidad = _localDbDefaul.ExecuteSprocAccessor("TEST_SELECT", EntidadFileMapper, Parameters);
+            return listEntidad.ToList();
+        }
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+    }
+}
---
+++ WebApiNetCoreEnterpriseLibrary/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNetCoreEnterpriseLibrary
+{
+    public class Usuario
+    {
+        public String CodRol { get; set; }
+        public String CodFuncion { get; set; }
+        public String DescripRol { get; set; }
+        public String EstadoRol { get; set; }
+        public String UsuarioCreacion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public String UsuarioEdicion { get; set; }
+        public DateTime FechaEdicion { get; set; }
+    }
+}
